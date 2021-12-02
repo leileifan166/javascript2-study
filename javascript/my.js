d37660dec3e68d631bfefff46da9b166d95cc16e
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2021-12-02 22:19:42 +0900

    init
    これからも独学でjavascriptの勉強を進めていきたいと思います。

---
+++ javascript/my.js
@@ -0,0 +1,72 @@
+alert('如果我是dj你还爱我吗？');
+var temp;
+var apple1 = '赤いリンゴ';
+var apple2 = '青いリンゴ';
+temp = apple1;
+apple1 = apple2;
+apple2 = temp;
+console.log(apple1);
+console.log(apple2);
+console.log(temp);
+
+var num =3>5;
+console.log(num);
+
+var str = '我是一个"高富帅"程序员';
+var str1 ="我是一个'高富帅'程序员";
+//外双内单，外单内双
+
+alert('酷热难耐，火辣的太阳底下，我挺拔的身姿，成为了最为独特的风景。我审视四周，这里，是我的舞台，我就是天地间的王者，这一刻，我豪气冲天，终于大喊一声："收破烂啦～"');
+
+var str = 'my name is andy';
+console.log(str.length);//15
+
+var strMsg = '私はかっこいいプログラマです';
+console.log(strMsg.length);//11
+
+console.log('沙漠'+'骆驼');
+console.log('pink先生'+18);//pink先生18
+console.log(12 + 12);
+console.log('12' + 12);//1212 数值相加，字符相连
+
+console.log('私は今年'+25+'才です');
+
+var age = 18;
+console.log('pink先生'+ age +'才です');
+
+var age = prompt('あなたの年齢を入力してくだい');
+alert('あなたは今年' +age + '才ですね');
+
+var flag = true;
+var flag1 = false;
+console.log(flag + 1);//2
+console.log(flag1 + 1);//1
+
+var variable = undefined;
+
+var numm = 10;
+console.log(typeof numm);
+
+var ito = '智宏';
+console.log((typeof ito));
+alert(typeof ito);
+
+
+var a = 100;
+var c = a.toString();
+alert(typeof(c));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
